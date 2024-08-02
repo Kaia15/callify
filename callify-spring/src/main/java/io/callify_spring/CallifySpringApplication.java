ce1d7c82c855ebc3d2538720c1f94f86b0e5f056
Tran Ong <baotranongtran@gmail.com>
2024-08-02 09:13:59 -0500

    add Spring project

---
+++ callify-spring/src/main/java/io/callify_spring/CallifySpringApplication.java
@@ -0,0 +1,13 @@
+package io.callify_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallifySpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CallifySpringApplication.class, args);
+	}
+
+}
---
+++ callify-spring/src/main/java/io/callify_spring/controllers/HelloController.java
@@ -0,0 +1,12 @@
+package io.callify_spring.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping ("/")
+    public String Hello() {
+        return "hello!";
+    }
+}
